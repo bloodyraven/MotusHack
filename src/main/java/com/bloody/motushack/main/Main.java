99af309de7c6bea0937ab6c1e07d99d1151e3b03
Bloody <bloody.duj@gmail.com>
2022-04-13 12:18:13 +0200

    ajout menu

--- src/main/java/com/bloody/motushack/main/Main.java
+++ src/main/java/com/bloody/motushack/main/Main.java
@@ -1,6 +1,12 @@
 package com.bloody.motushack.main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 import com.bloody.motushack.model.AppModel;
@@ -11,13 +17,35 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
 		f.setTitle("MotusHack");
-		String length = JOptionPane.showInputDialog(null, "Taille du mot", "", JOptionPane.QUESTION_MESSAGE);
-		String firstLetter = JOptionPane.showInputDialog(null, "Première lettre du mot", "", JOptionPane.QUESTION_MESSAGE);
-		f.setContentPane(new AppPanel(new AppModel(),Integer.parseInt(length),firstLetter));
+		setContentAndAskInputs(f);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(1000, 800);
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
+		
+		JMenuBar menubar = new JMenuBar();
+		JMenu menu = new JMenu("Menu");
+		JMenuItem jmi = new JMenuItem("Nouveau");
+		
+		jmi.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				f.setVisible(false);
+				setContentAndAskInputs(f);
+				f.setVisible(true);
+			}
+		});
+		
+		menubar.add(menu);
+		menu.add(jmi);
+		
+		f.setJMenuBar(menubar);
+	}
+	
+	public static void setContentAndAskInputs(JFrame f) {
+		String length = JOptionPane.showInputDialog(null, "Taille du mot", "", JOptionPane.QUESTION_MESSAGE);
+		String firstLetter = JOptionPane.showInputDialog(null, "Première lettre du mot", "", JOptionPane.QUESTION_MESSAGE);
+		f.setContentPane(new AppPanel(new AppModel(),Integer.parseInt(length),firstLetter));
 	}
 	
 }
